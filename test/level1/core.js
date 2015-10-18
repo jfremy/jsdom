06a7b6a6cab70c08e257c3499f69945e285749f5
Domenic Denicola <d@domenic.me>
2015-10-18 19:57:26 -0400

    Test fixes. insertBefore fixes. Attr on window now.

--- test/level1/core.js
+++ test/level1/core.js
@@ -12793,45 +12793,6 @@ exports.tests = {
   /**
    *
 
-   The "getNodeType()" method for an Attribute Node
-
-   returns the constant value 2.
-
-
-
-   Retrieve the first attribute from the last child of
-
-   the first employee and invoke the "getNodeType()"
-
-   method.   The method should return 2.
-
-
-   * @author NIST
-   * @author Mary Brady
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-core#ID-111237558
-   */
-  nodeattributenodetype: function(test) {
-    var success;
-    var doc;
-    var elementList;
-    var testAddr;
-    var addrAttr;
-    var nodeType;
-
-    doc = staff.staff();
-    elementList = doc.getElementsByTagName("address");
-    testAddr = elementList.item(0);
-    addrAttr = testAddr.getAttributeNode("domestic");
-    nodeType = addrAttr.nodeType;
-
-    test.equal(nodeType, 2, 'nodeAttrNodeTypeAssert1');
-
-    test.done();
-  },
-
-  /**
-   *
-
    The string returned by the "getNodeValue()" method for an
 
    Attribute Node is the value of the Attribute.
@@ -14476,56 +14437,6 @@ exports.tests = {
     childName = child.nodeName;
 
     test.equal(childName, "newChild2", 'childName4');
-
-    test.done();
-  },
-
-  /**
-   *
-   The "insertBefore(newChild,refChild)" method raises a
-   HIERARCHY_REQUEST_ERR DOMException if this node is of
-   a type that does not allow children of the type "newChild"
-   to be inserted.
-
-   Retrieve the root node and attempt to insert a newly
-   created Attr node.   An Element node cannot have children
-   of the "Attr" type, therefore the desired exception
-   should be raised.
-
-   * @author NIST
-   * @author Mary Brady
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-core#xpointer(id('ID-258A00AF')/constant[@name='HIERARCHY_REQUEST_ERR'])
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-core#ID-952280727
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-core#xpointer(id('ID-952280727')/raises/exception[@name='DOMException']/descr/p[substring-before(.,':')='HIERARCHY_REQUEST_ERR'])
-   * @see http://www.w3.org/TR/1998/REC-DOM-Level-1-19981001/level-one-core#ID-952280727
-   * @see http://www.w3.org/Bugs/Public/show_bug.cgi?id=249
-   */
-  nodeinsertbeforeinvalidnodetype: function(test) {
-    var success;
-    var doc;
-    var rootNode;
-    var newChild;
-    var elementList;
-    var refChild;
-    var insertedNode;
-
-    doc = staff.staff();
-    rootNode = doc.documentElement;
-
-    newChild = doc.createAttribute("newAttribute");
-    elementList = doc.getElementsByTagName("employee");
-    refChild = elementList.item(1);
-
-    {
-      success = false;
-      try {
-        insertedNode = rootNode.insertBefore(newChild,refChild);
-      }
-      catch(ex) {
-        success = (typeof(ex.code) != 'undefined' && ex.code == 3);
-      }
-      test.ok(success, 'throw_HIERARCHY_REQUEST_ERR');
-    }
 
     test.done();
   },
